ce45616360cc2915c6df43529354e4f9d1d9d39a
deyvi <deyviyairtasaycomendoza@gmail.com>
2026-04-13 09:26:31 -0500

    Vista Transparencia

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
+import { Inicio} from './pages/inicio/inicio';
+import { Footer } from './layout/footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Inicio, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+
 }
