4600d9455c65c996f02e769856cf77224e8e8864
MrAron7w7 <magallanestorresaron@gmail.com>
2026-04-14 15:09:11 -0400

    Aregland rutas con nombres

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,15 +5,13 @@ import { Noticias } from './pages/noticias/noticias';
 import { Programas } from './pages/programas/programas';
 import { Transparencia } from './pages/transparencia/transparencia';
 import { Admision } from './pages/admision/admision';
-import { Becas } from './pages/becas/becas';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'inicio', pathMatch: 'full'},
-    {path: 'inicio', component: Inicio},
-    {path: 'nosotros', component: Nosotros},
-    {path: 'noticias', component: Noticias},
-    {path: 'programas', component: Programas},
-    {path: 'admision', component: Admision},
-    {path: 'transparencia', component: Transparencia},
-    {path: 'becas', component: Becas},
+    {path: 'inicio', component: Inicio, title: 'Inicio'},
+    {path: 'nosotros', component: Nosotros, title: 'Nosotros'},
+    {path: 'noticias', component: Noticias, title: 'Noticias'},
+    {path: 'programas', component: Programas, title: 'Programas'},
+    {path: 'admision', component: Admision, title: 'Admision'},
+    {path: 'transparencia', component: Transparencia, title: 'Transparencia'},
 ];
