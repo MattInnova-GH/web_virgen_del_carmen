bcefcb86c0256124e3e4194dcdaf459b26f830d6
Jesús Silva Morales <jesusalbertosilvamorales@gmail.com>
2026-04-17 17:10:39 -0500

    Se eliminó los enlaces erroneos para la visualización de inversiones y procedimientos.

--- frontend/src/app/pages/procedimientos/procedimientos.ts
+++ frontend/src/app/pages/procedimientos/procedimientos.ts
@@ -12,17 +12,17 @@ export class Procedimientos {
     {
       id: '1',
       label: 'TUPA',
-      pdfUrl: 'assets/pdfs/PROTOCOLO-DE-ADMISION-2026.pdf',
+      pdfUrl: '',
     },
     {
       id: '2',
       label: 'PLAN DE TRABAJO ANUAL',
-      pdfUrl: 'assets/pdfs/REGLAMENTO-DE-ADMISION-2026-1.pdf',
+      pdfUrl: '',
     },
     {
       id: '3',
       label: 'MANUAL DE PROCESOS INSTITUCIONALES',
-      pdfUrl: 'assets/pdfs/PROSPECTO-DE-ADMISION-2026.pdf',
+      pdfUrl: '',
     },
   ];
 }
