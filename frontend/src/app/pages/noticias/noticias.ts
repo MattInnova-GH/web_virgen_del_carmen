8146443075e63808430e5dd73a70dd3001872d59
Justin Aguilar <justin.aguilar@upsjb.edu.pe>
2026-04-30 14:27:53 -0500

    Actualicé el paginado en seccion de noticias

--- frontend/src/app/pages/noticias/noticias.ts
+++ frontend/src/app/pages/noticias/noticias.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, inject, signal, computed } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
@@ -16,6 +16,29 @@ export class Noticias implements OnInit {
 
   featuredNoticias = signal<any[]>([]);
   otrasNoticias = signal<any[]>([]);
+
+  // Paginación
+  readonly pageSize = 5;
+  readonly currentPage = signal(1);
+
+  readonly totalPages = computed(() =>
+    Math.ceil(this.otrasNoticias().length / this.pageSize)
+  );
+
+  readonly paginatedNoticias = computed(() => {
+    const start = (this.currentPage() - 1) * this.pageSize;
+    return this.otrasNoticias().slice(start, start + this.pageSize);
+  });
+
+  readonly pages = computed(() =>
+    Array.from({ length: this.totalPages() }, (_, i) => i + 1)
+  );
+
+  goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages()) {
+      this.currentPage.set(page);
+    }
+  }
 
   ngOnInit(): void {
     this.http.get<any[]>(`${this.api}/news/list`).subscribe({
